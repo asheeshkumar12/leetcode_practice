13900e167bb38a416d2a7124eddd5f53530e0596
Asheesh Kumar <91361484+asheeshkumar12@users.noreply.github.com>
2022-04-23 00:00:38 +0530

    Create DesignHashMap.cpp

---
+++ DesignHashMap.cpp
@@ -0,0 +1,34 @@
+class MyHashMap {
+public:
+    vector<int>* v;
+    MyHashMap() {
+        v=new vector<int>(1e6+9,-1);
+        
+    }
+    
+    void put(int key, int value) {
+        
+        if(v->size()<key){
+            v->resize(key+1,-1);
+        }
+        (*v)[key]=value;
+        
+    }
+    
+    int get(int key){
+      return (*v)[key];
+    }
+    
+    void remove(int key) {
+        (*v)[key]=-1;
+        
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
