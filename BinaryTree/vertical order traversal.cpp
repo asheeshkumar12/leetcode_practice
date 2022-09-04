a38967344f312ab720ffcf30c78eb645af618242
Asheesh Kumar <91361484+asheeshkumar12@users.noreply.github.com>
2022-09-05 01:34:42 +0530

    Update vertical order traversal.cpp

--- BinaryTree/vertical order traversal.cpp
+++ BinaryTree/vertical order traversal.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Solution 1
 class Solution {
 public:
     map<int,vector<int>> m;
@@ -49,3 +51,54 @@ public:
         return vvv;
     }
 };
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Solution 2:
+
+
+map < int, map < int, multiset < int >>> nodes;
+  queue < pair < node * , pair < int, int >>> todo;
+  todo.push({
+    root,
+    {
+      0,
+      0
+    }
+  }); //initial vertical and level
+  while (!todo.empty()) {
+    auto p = todo.front();
+    todo.pop();
+    node * temp = p.first;
+
+    //x -> vertical , y->level
+    int x = p.second.first, y = p.second.second;
+    nodes[x][y].insert(temp -> data); //inserting to multiset
+
+    if (temp -> left) {
+      todo.push({
+        temp -> left,
+        {
+          x - 1,
+          y + 1
+        }
+      });
+    }
+    if (temp -> right) {
+      todo.push({
+        temp -> right,
+        {
+          x + 1,
+          y + 1
+        }
+      });
+    }
+  }
+  vector < vector < int >> ans;
+  for (auto p: nodes) {
+    vector < int > col;
+    for (auto q: p.second) {
+      col.insert(col.end(), q.second.begin(), q.second.end());
+    }
+    ans.push_back(col);
+  }
+  return ans;
+}
