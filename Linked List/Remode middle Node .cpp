fe37ee1c4c9514deabf8d8dcfe35937043e9a48d
Asheesh Kumar <91361484+asheeshkumar12@users.noreply.github.com>
2022-10-15 02:48:42 +0530

    Create Remode middle Node .cpp

---
+++ Linked List/Remode middle Node .cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(!head || !head->next){
+            return NULL;
+        }
+        ListNode* slow=head;
+       // ListNode* newNode=slow;
+        ListNode* fast=head->next->next;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        if(slow->next->next){
+            slow->next=slow->next->next;
+        }
+        else{
+            slow->next=NULL;
+        }
+        return head;
+    }
+};
